8edaf204693b2c687a2b34cddb55dc61e17aa4a8
Ivaylo Chernev <ivko0527@gmail.com>
2025-01-11 16:11:28 +0200

    Update remove empty folders

--- Scrypt.py
+++ Scrypt.py
@@ -3,6 +3,9 @@ from os.path import isfile, join, isdir
 import os
 import shutil
 
+def is_folder_empty(folder_path):
+    return len(listdir(folder_path)) == 0
+
 file_path = r"C:\Users\ivko0\Downloads"
 files = [f for f in listdir(file_path) if isfile(join(file_path, f))]
 
@@ -25,3 +28,9 @@ for file in files:
     else:
         new_dest_file = join(folder_name, f"{filename}_i.{filetype}")
         shutil.move(src_file, new_dest_file)
+
+# Remove empty folders
+for folder in listdir(file_path):
+    folder_path = join(file_path, folder)
+    if isdir(folder_path) and is_folder_empty(folder_path):
+        os.rmdir(folder_path)
